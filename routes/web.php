99c66b3f31a9d8db0a6e1fd936730672e97a2965
unknown <rivapermana763@gmail.com>
2024-12-08 23:56:58 +0700

    dah

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use App\Http\Controllers\TasksController;
+
+Route::get('/', [TasksController::class, 'index']);
+Route::post('/tasks', [TasksController::class, 'store']);
+Route::get('/tasks/{id}/edit', [TasksController::class, 'edit']);
+Route::put('/tasks/{id}', [TasksController::class, 'update']);
+Route::delete('/tasks/{id}', [TasksController::class, 'destroy']);
